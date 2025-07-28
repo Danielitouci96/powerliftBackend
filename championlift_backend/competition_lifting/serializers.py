92959c0d90a750e8c56bf9e13edde7bfb2bf98b5
daniDELL <danielitouci96@gmail.com>
2025-07-27 21:02:45 -0400

    mostrar el total de peso levantado

--- championlift_backend/competition_lifting/serializers.py
+++ championlift_backend/competition_lifting/serializers.py
@@ -12,14 +12,43 @@ class LiftSerializer(serializers.ModelSerializer):
 class CompetitorSerializer(serializers.ModelSerializer):
     lift_history = LiftSerializer(many=True, read_only=True)
     latest_lift = LiftSerializer(read_only=True)
+    best_total = serializers.SerializerMethodField()
 
     class Meta:
         model = Competitor
         fields = [
             'id', 'name', 'age', 'weight', 'height',
             'weight_class', 'profile_image', 'lift_history',
-            'latest_lift', 'ipf_points', 'gender'
+            'latest_lift', 'ipf_points', 'gender', 'best_total'
         ]
+    
+    def get_best_total(self, obj):
+        """
+        Obtiene los mejores levantamientos válidos para squat, bench y deadlift.
+        Nombres normalizados a minúsculas.
+        """
+        lift_history = obj.lift_history.filter(valid='valid')
+        squat_names = {'cuclilla', 'squad', 'squat', 'cuclilla(squad)'}
+        bench_names = {'press de banca', 'bench', 'bench press'}
+        deadlift_names = {'peso muerto', 'deadlift'}
+
+        best = {'squat': 0.0, 'bench': 0.0, 'deadlift': 0.0}
+
+        for lift in lift_history:
+            if lift.valid != 'valid':
+                continue
+            lname = lift.name.strip().lower()
+            if lname in squat_names:
+                best['squat'] = max(best['squat'], lift.weight)
+            elif lname in bench_names:
+                best['bench'] = max(best['bench'], lift.weight)
+            elif lname in deadlift_names:
+                best['deadlift'] = max(best['deadlift'], lift.weight)
+        
+        sum_best = best['squat'] + best['bench'] + best['deadlift']
+
+        return sum_best
+
 
 
 class ModalitySerializer(serializers.ModelSerializer):
